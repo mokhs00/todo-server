0c76db4c9d0b3abb6ffb396901671eff129d9922
mokhs00 <mokhs00@naver.com>
2021-04-14 00:52:09 +0900

    add : SpringApplication.run()

--- src/main/java/org/example/TodoServerApplication.java
+++ src/main/java/org/example/TodoServerApplication.java
@@ -1,13 +1,15 @@
 package org.example;
 
+import org.example.model.TodoEntity;
+import org.example.repository.TodoRepository;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TodoServerApplication{
 
     public static void main(String[] args) {
-
-
+        SpringApplication.run(TodoServerApplication.class, args);
     }
 
 }

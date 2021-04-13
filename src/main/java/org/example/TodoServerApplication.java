7883dea1aa4f725b605ba987a7ad90d52d44427b
mokhs00 <mokhs00@naver.com>
2021-04-14 00:11:01 +0900

    initial commit

---
+++ src/main/java/org/example/TodoServerApplication.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoServerApplication{
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
